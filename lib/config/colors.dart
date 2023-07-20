2738486d7ac3a811d1e9fa2440fac8791122d60e
ImportAd <musin.f.mail@gmail.com>
2023-07-20 22:45:47 +0300

    feature
    added a separate file for rendering application colors

---
+++ lib/config/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const backgroundBlack = Color.fromRGBO(31, 41, 51, 1);
+  static const white = Color.fromRGBO(245, 247, 250, 1);
+  static const black = Color.fromRGBO(0, 0, 0, 1);
+}
