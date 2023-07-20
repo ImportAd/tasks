a10b1c34a639203d4ff3d5eaf3b333a5abf5214b
ImportAd <musin.f.mail@gmail.com>
2023-07-20 22:45:07 +0300

    feature
    added a separate file for rendering constants

---
+++ lib/config/constants.dart
@@ -0,0 +1,8 @@
+class AppConstants {
+  static const String testSummary = "Краткое описание задачи";
+  static const String testHashtag = "#Универ";
+  static const String testTime = "20:00";
+  static const String name = "Алина";
+  static const String birthday = "18 февраля";
+  static const String year = "21 год";
+}
