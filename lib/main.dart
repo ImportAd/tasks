e0d687357b4668d10606aa35df1c8eb109eb40af
ImportAd <musin.f.mail@gmail.com>
2023-08-02 14:25:28 +0300

    chore

--- lib/main.dart
+++ lib/main.dart
@@ -3,23 +3,39 @@ import 'package:flutter/material.dart';
 import 'package:tasks/config/theme.dart';
 import 'package:tasks/UI/screens/home/home.dart';
 
-// void main() {
-//   runApp(const MyApp());
-// }
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      // инициализация get для работы get
+      debugShowCheckedModeBanner: false,
+      theme: theme(),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+// не работотает
+// просто не может запуситься
+// ToDo: Нормально разобраться с ScreenUtili
 
 //   @override
 //   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
-//       ),
-//       home: const MyHomePage(),
-//     );
+//     return ScreenUtilInit(
+//         // инициализация экрана для пиксельпевфекта
+//         designSize: const Size(390, 844),
+//         minTextAdapt: true,
+//         splitScreenMode: true,
+//         builder: (context, child) {
+//           return GetMaterialApp(
+//             // инициализация get для работы get
+//             debugShowCheckedModeBanner: false,
+//             theme: theme(),
+//             home: const MyHomePage(),
+//           );
+//         });
 //   }
 // }
 
-void main() => runApp(GetMaterialApp(theme: theme(), home: const MyHomePage()));
