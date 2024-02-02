4c2f015e66caf0eebd4ea14560e1664e9779bf1a
ImportAd <musin.f.mail@gmail.com>
2024-02-02 16:22:10 +0300

    start of development of a new screen

---
+++ lib/screens/tasks/tasks_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Tasks extends StatelessWidget {
+  const Tasks({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Title'),
+      ),
+      body: Container(),
+    );
+  }
+}
